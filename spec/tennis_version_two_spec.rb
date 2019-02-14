07bcdb73ca2b087588c8e0ed19e03632a2b2fd82
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-14 15:15:23 +0000

    test: add tests for win and advantage

--- spec/tennis_version_two_spec.rb
+++ spec/tennis_version_two_spec.rb
@@ -22,6 +22,11 @@ describe TennisVersionTwo do
       3.times { tennis.win_point(:player_one) }
       expect(tennis.score).to eq('Fourty-Love')
     end
+
+    it 'gives a win to Player One when they win four points' do
+      4.times { tennis.win_point(:player_one) }
+      expect(tennis.score).to eq('Player One wins!')
+    end
   end
 
   context 'when Player Two only scores' do
@@ -38,6 +43,70 @@ describe TennisVersionTwo do
     it 'gives a score of Love-Fourty when Player Two wins three points' do
       3.times { tennis.win_point(:player_two) }
       expect(tennis.score).to eq('Love-Fourty')
+    end
+
+    it 'gives a win to Player Two when they win four points' do
+      4.times { tennis.win_point(:player_two) }
+      expect(tennis.score).to eq('Player Two wins!')
+    end
+  end
+
+  context 'when deuce' do
+    before do
+      3.times do
+        tennis.win_point(:player_one)
+        tennis.win_point(:player_two)
+      end
+    end
+
+    it 'gives a score of Deuce when each player wins three points and the scores are equal' do
+      expect(tennis.score).to eq('Deuce')
+    end
+
+    it 'gives a score of Advantage Player One if Player Two wins three points and Player One wins four points' do
+      tennis.win_point(:player_one)
+      expect(tennis.score).to eq('Advantage Player One')
+    end
+
+    it 'gives a score of Advantage Player Two if Player One wins three points and Player Two wins four points' do
+      tennis.win_point(:player_two)
+      expect(tennis.score).to eq('Advantage Player Two')
+    end
+  end
+
+  context 'when advantage' do
+    it 'gives a score of Deuce if Player One loses their advantage' do
+      4.times do
+        tennis.win_point(:player_one)
+        tennis.win_point(:player_two)
+      end
+      expect(tennis.score).to eq('Deuce')
+    end
+
+    it 'gives a score of Deuce if Player Two loses their advantage' do
+      4.times do
+        tennis.win_point(:player_two)
+        tennis.win_point(:player_one)
+      end
+      expect(tennis.score).to eq('Deuce')
+    end
+
+    it 'gives a win to Player One if they win a point from advantage' do
+      3.times do
+        tennis.win_point(:player_one)
+        tennis.win_point(:player_two)
+      end
+      2.times { tennis.win_point(:player_one) }
+      expect(tennis.score).to eq('Player One wins!')
+    end
+
+    it 'gives a win to Player two if they win a point from advantage' do
+      3.times do
+        tennis.win_point(:player_one)
+        tennis.win_point(:player_two)
+      end
+      2.times { tennis.win_point(:player_two) }
+      expect(tennis.score).to eq('Player Two wins!')
     end
   end
 end
